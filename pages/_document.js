bb0003222f3287f959dc37e4ddca8f66a3543a13
thegera4 <84020433+thegera4@users.noreply.github.com>
2023-03-16 21:11:07 -0600

    "ads config"

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1515388210457824"
+          crossorigin="anonymous"
+        >
+        </script>
+      </Head>
       <body>
         <Main />
         <NextScript />
---
+++ pages/ads.txt.js
@@ -0,0 +1,12 @@
+//mostrar info de ads
+import React from 'react'
+
+function ads() {
+  return (
+    <div>
+    google.com, pub-1515388210457824, DIRECT, f08c47fec0942fa0
+    </div>
+  )
+}
+
+export default ads
